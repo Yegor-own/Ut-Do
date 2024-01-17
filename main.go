d3934d4d5cedb046f2016f3a96e881bcfef7aebd
Yegor-own <egor891712@gmail.com>
2024-01-17 20:25:09 +0500

    init

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+func main() {
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "add",
+				Usage: "add a task to the to-do list",
+				Action: func(context *cli.Context) error {
+					fmt.Println("added task:", context.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
